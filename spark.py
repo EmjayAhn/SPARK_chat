5e1dac9010bfbb4bcc3ca34bffd3c99226110e61
EmjayAhn <emjay.data@gmail.com>
2018-12-18 15:38:31 +0900

    Feat: Add spark.py

--- spark.py
+++ spark.py
@@ -18,12 +18,10 @@ def request(message):
     to_client = dict()
     if message == 'new_connect':
         to_client['message'] = '유저가 입장하였습니다.'
-        to_client['type']
-
+        to_client['type'] = 'connect'
     else:
         to_client['message'] = message
         to_client['type'] = 'normal'
-
     send(to_client, broadcast=True)
 
 if __name__ == '__main__':
