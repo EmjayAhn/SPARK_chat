e9a3049e013e618ce5106ac7bc484b9c1030e947
EmjayAhn <emjay.data@gmail.com>
2018-12-18 15:06:14 +0900

    Feat: add spark.py

--- spark.py
+++ spark.py
@@ -1,5 +1,12 @@
 from flask import *
+from flask_socketio import SocketIO, send
+
 app = Flask(__name__)
+socket_io = SocketIO(app)
+
+
+if __name__ == '__main__':
+    socket_io.run(app, debug=True)
 
 @app.route("/")
 def home():
@@ -9,4 +16,16 @@ def home():
 def chat():
     return render_template('chat.html')
 
-app.run()
+@socket_io.on("message")
+def request(message):
+    print("message : " + message)
+    to_client = dict()
+    if message == 'new_connect':
+        to_client['message'] = '유저가 입장하였습니다.'
+        to_client['type']
+
+    else:
+        to_client['message'] = message
+        to_client['type'] = 'normal'
+
+    send(to_client, broadcast=True)
