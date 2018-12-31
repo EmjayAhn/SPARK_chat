d351b149d5d4500ad655067b65054796a872b310
민정 <minn@minjeong-ui-MacBook-Pro.local>
2018-12-31 13:32:25 +0900

    Feat: merge from origin to local

--- spark.py
+++ spark.py
@@ -1,12 +1,35 @@
-from flask import *
+from flask import Flask, jsonify, render_template
+from subprocess import call
+from flask_socketio import SocketIO, send
+
 app = Flask(__name__)
+app.secret_key = "mysecret"
 
-@app.route("/")
-def home():
-    return render_template('index.html')
+socket_io = SocketIO(app)
 
-@app.route("/chat")
-def chat():
+@app.route('/')
+def hello_world():
+    return "Hello Gaemigo Project Home Page!!"
+
+@app.route('/chat')
+def chatting():
     return render_template('chat.html')
 
-app.run()
+
+@socket_io.on("message")
+def request(message):
+    print("message : "+ message)
+    to_client = dict()
+    if message == 'new_connect':
+        to_client['message'] = "새로운 유저가 입장하였습니다."
+        to_client['type'] = 'connect'
+    else:
+        to_client['message'] = message
+        to_client['type'] = 'normal'
+    # emit("response", {'data': message['data'], 'username': session['username']}, broadcast=True)
+    send(to_client, broadcast=True)
+
+
+
+if __name__ == '__main__':
+    socket_io.run(app, debug=True, port=9999)
