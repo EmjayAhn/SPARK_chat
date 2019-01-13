b286cf3e9d6be938c793ebb839668cd3372504b4
민정 <minn@minjeong-ui-MacBook-Pro.local>
2019-01-13 16:56:45 +0900

    Feat: get user_id & user_icon

--- spark.py
+++ spark.py
@@ -1,40 +1,3 @@
-<<<<<<< HEAD
-from flask import Flask, jsonify, render_template
-from subprocess import call
-from flask_socketio import SocketIO, send
-
-app = Flask(__name__)
-app.secret_key = "mysecret"
-
-socket_io = SocketIO(app)
-
-@app.route('/')
-def hello_world():
-    return "Hello Gaemigo Project Home Page!!"
-
-@app.route('/chat')
-def chatting():
-    return render_template('chat.html')
-
-
-@socket_io.on("message")
-def request(message):
-    print("message : "+ message)
-    to_client = dict()
-    if message == 'new_connect':
-        to_client['message'] = "새로운 유저가 입장하였습니다."
-        to_client['type'] = 'connect'
-    else:
-        to_client['message'] = message
-        to_client['type'] = 'normal'
-    # emit("response", {'data': message['data'], 'username': session['username']}, broadcast=True)
-    send(to_client, broadcast=True)
-
-
-
-if __name__ == '__main__':
-    socket_io.run(app, debug=True, port=9999)
-=======
 from flask import *
 from flask_socketio import SocketIO, send
 
@@ -43,6 +6,7 @@ socket_io = SocketIO(app)
 
 @app.route("/")
 def home():
+
     return render_template('index.html')
 
 @app.route("/chat")
@@ -54,7 +18,7 @@ def request(message):
     print("message : " + message)
     to_client = dict()
     if message == 'new_connect':
-        to_client['message'] = '유저가 입장하였습니다.'
+        to_client['message'] = '[{}]님이 입장하였습니다.'.format(user_id)
         to_client['type'] = 'connect'
     else:
         to_client['message'] = message
@@ -63,4 +27,3 @@ def request(message):
 
 if __name__ == '__main__':
     socket_io.run(app, debug=True)
->>>>>>> d15d4250b7da8af62a8f78bcd2b2bd8624643351
