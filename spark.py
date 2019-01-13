cfdf67af761ddbcf32e7262efeab9cc33e82c95b
민정 <minn@minjeong-ui-MacBook-Pro.local>
2019-01-13 19:15:33 +0900

    Feat: getting icon and user id to ctat.html

--- spark.py
+++ spark.py
@@ -1,4 +1,5 @@
-from flask import *
+from flask import Flask
+from flask import render_template, url_for, request, redirect
 from flask_socketio import SocketIO, send
 
 app = Flask(__name__)
@@ -9,21 +10,33 @@ def home():
 
     return render_template('index.html')
 
+
+@app.route('/form', methods=['GET', 'POST'])
+def form():
+    global glon
+    if request.method == 'POST':
+        glon = request.form['glon']
+        icon = request.form['icon']
+        return render_template('chat.html', glon=glon, icon=icon)
+
 @app.route("/chat")
 def chat():
     return render_template('chat.html')
 
 @socket_io.on("message")
-def request(message):
+def msg_send(message):
+    global glon
     print("message : " + message)
     to_client = dict()
     if message == 'new_connect':
-        to_client['message'] = '[{}]님이 입장하였습니다.'.format(user_id)
+        to_client['message'] = '[{}]님이 입장하였습니다.'.format(glon)
         to_client['type'] = 'connect'
     else:
-        to_client['message'] = message
+        to_client['message'] = glon + ' : ' + message
         to_client['type'] = 'normal'
     send(to_client, broadcast=True)
 
+
 if __name__ == '__main__':
-    socket_io.run(app, debug=True)
+	app.run(debug=True)
+	socket_io.run(app, debug=True)
