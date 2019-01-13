0c15259889f8af14c0f82e4020f831626daf3e55
dbsltm <eunkyu0322@gmail.com>
2019-01-13 17:26:09 +0900

    Feat: all

--- spark.py
+++ spark.py
@@ -1,4 +1,5 @@
-from flask import *
+from flask import Flask
+from flask import render_template, url_for, request, redirect
 from flask_socketio import SocketIO, send
 
 app = Flask(__name__)
@@ -9,21 +10,39 @@ def home():
 
     return render_template('index.html')
 
-@app.route("/chat")
-def chat():
-    return render_template('chat.html')
+# @app.route("/chat")
+# def chat():
+# 	# animal = "dog"
+#     return render_template('chat.html', value="eunice", img="picture")
+#
+# @app.route("/chat", methods=['POST'])
+# def payment():
+# 	return render_template('index.html', value=animal)
 
+
+@app.route('/form', methods=['GET', 'POST'])
+def form():
+    if request.method == 'POST':
+        id = request.form['user_id']
+        return render_template('chat.html', id=id)
+#
 @socket_io.on("message")
-def request(message):
+def msg_new(message):
     print("message : " + message)
     to_client = dict()
     if message == 'new_connect':
+<<<<<<< HEAD
         to_client['message'] = '[{}]님이 입장하였습니다.'.format(user_id)
+=======
+        to_client['message'] = '가 입장하였습니다.'
+>>>>>>> Feat: index and chat connected
         to_client['type'] = 'connect'
     else:
         to_client['message'] = message
         to_client['type'] = 'normal'
     send(to_client, broadcast=True)
 
+
 if __name__ == '__main__':
-    socket_io.run(app, debug=True)
+	app.run(debug=True)
+	socket_io.run(app, debug=True)
