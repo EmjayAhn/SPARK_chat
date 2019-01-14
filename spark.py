17e226b71bf2f72528d8d26e2987f8a639a53f06
민정 <minn@minjeong-ui-MacBook-Pro.local>
2019-01-14 17:25:05 +0900

    Feat: minjung edit

--- spark.py
+++ spark.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask import render_template, url_for, request, redirect
-from flask_socketio import SocketIO, send
+from flask_socketio import SocketIO, send, leave_room
 
 app = Flask(__name__)
 socket_io = SocketIO(app)
@@ -11,32 +11,34 @@ def home():
     return render_template('index.html')
 
 
-@app.route('/form', methods=['GET', 'POST'])
-def form():
+@app.route('/chat', methods=['GET', 'POST'])
+def chat():
     global username, profilecon
     if request.method == 'POST':
         username = request.form['username']
         profilecon = request.form['profilecon']
         return render_template('chat.html', username=username, profilecon=profilecon)
 
-@app.route("/chat")
-def chat():
-    return render_template('chat.html')
-
 @socket_io.on("message")
 def msg_send(message):
     global username, profilecon
-    print("message : " + message)
-    to_client = dict()
+    users = dict()
     if message == 'new_connect':
-        to_client['message'] = '[{}]님이 입장하였습니다.'.format(username)
-        to_client['profilecon'] = profilecon
-        to_client['type'] = 'connect'
+        users['message'] = '[{}]님이 입장하였습니다.'.format(username)
+        users['profilecon'] = profilecon
+        users['type'] = 'connect'
     else:
-        to_client['message'] = username + ' : ' + message
-        to_client['profilecon'] = profilecon
-        to_client['type'] = 'normal'
-    send(to_client, broadcast=True)
+        users['message'] = '[' + username + ']' + ' : ' + message
+        users['profilecon'] = profilecon
+        users['type'] = 'normal'
+    send(users, broadcast=True)
+
+# @socket_io.on("leave")
+# def on_leave(data):
+#     username = data['username']
+#     room = data['room']
+#     leave_room(room)
+#     send(username + ' has left the room.', room=room)
 
 
 if __name__ == '__main__':
